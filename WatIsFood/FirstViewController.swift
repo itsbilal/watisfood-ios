fc26c2a938b8d7df5ba64aa9a56ead83e68a68eb
Bilal Akhtar <me@itsbilal.com>
2014-11-21 21:34:15 -0500

    Initial Commit

---
+++ WatIsFood/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WatIsFood
+//
+//  Created by Bilal Akhtar on 2014-11-21.
+//  Copyright (c) 2014 Bilal Akhtar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WatIsFood/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WatIsFood
+//
+//  Created by Bilal Akhtar on 2014-11-21.
+//  Copyright (c) 2014 Bilal Akhtar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
